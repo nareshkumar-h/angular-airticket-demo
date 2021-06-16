a44361b9b9cc8d39cad740dfbdff4a49f1dd7100
Naresh Kumar H <nareshkumarh@live.com>
2021-06-16 13:37:46 +0530

    Added guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AuthGuard } from './auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RoleGuard } from './role.guard';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  { path:'admindashboard', component: AdminDashboardComponent},
+  { path:'admindashboard', component: AdminDashboardComponent, canActivate:[AuthGuard, RoleGuard]},
   { path:'home', component: HomeComponent},
   { path:'login', component: LoginComponent},
   { path:'signup', component: SignupComponent},
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router:Router){
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      let user = localStorage.getItem("LOGGED_IN_USER");
+      if (!user){
+        this.router.navigate(['login']);
+      }
+    return true;
+  }
+
+}
